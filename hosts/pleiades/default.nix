64bf64ec0fc69b1639ef6372e51ee2b1a9fad100
Greg Schwimer <schwim@bitrail.com>
2026-06-17 14:04:52 -0700

    switch to kde for pleiades, fixed :

--- hosts/pleiades/default.nix
+++ hosts/pleiades/default.nix
@@ -83,7 +83,7 @@
   my.desktop.gnome.enable      = true;
   my.desktop.kde.enable        = false;
   my.desktop.displayManager    = "sddm";   # KDE's login manager
-:
+
   my.services.xrdp.enable      = true;
   my.services.incus.enable     = true;       # flip off when laptop leaves the cluster
   my.services.incus.cluster.enable = false; # turning off until we need it
